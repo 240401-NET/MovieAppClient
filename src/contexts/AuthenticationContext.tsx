ba665b9ea3027436401c9c87edd1b8f43933f781
Michael Phan <michaelphan35@gmail.com>
2024-05-23 17:40:03 -0700

    removed unused lines from auth context

--- src/contexts/AuthenticationContext.tsx
+++ src/contexts/AuthenticationContext.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, createContext, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   UserLogin,
-  UserLogout,
   UserRegistration,
 } from "../services/UserServices";
 
